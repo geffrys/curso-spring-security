4aa557a79de76dc71d7b9e58c329d3c74bbb9b3b
Geffry Alejandro Ospina Atehortúa <geffry.ospina@gmail.com>
2024-04-21 00:04:23 -0500

    Add: More request matchers based on userRole, my own in memory users with specific roles

--- Backend/src/main/java/com/platzi/pizza/web/config/BasicAuth.java
+++ Backend/src/main/java/com/platzi/pizza/web/config/BasicAuth.java
@@ -4,18 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BasicAuth {
+    private final String PIZZAS_ENDPOINT = "/api/pizzas/**";
+    private final String ORDERS_ENDPOINT = "/api/orders/**";
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
                 customized -> {
                     customized
-                            .requestMatchers(HttpMethod.GET,"/api/pizzas/**").permitAll()
-                            .requestMatchers(HttpMethod.PUT, "/api/pizzas/**").denyAll()
+                            .requestMatchers(HttpMethod.GET, PIZZAS_ENDPOINT).hasAnyRole("ADMIN","CUSTOMER")
+                            .requestMatchers(HttpMethod.POST, PIZZAS_ENDPOINT).hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.PUT, PIZZAS_ENDPOINT).hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.GET, ORDERS_ENDPOINT).hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.POST, ORDERS_ENDPOINT ).hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.PUT, ORDERS_ENDPOINT).hasRole("ADMIN")
                             .anyRequest()
                             .authenticated();
                 }
@@ -25,5 +39,25 @@ public class BasicAuth {
                 Customizer.withDefaults()
         );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        UserDetails customer = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("customer"))
+                .roles("CUSTOMER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
